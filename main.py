bca39f2dc7694d6924eca2ee66519f4701602b4b
muthomi254 <dmunene202020@gmail.com>
2024-03-07 13:34:56 +0300

    (ft): Importing auth blueprints

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from app.models import db, User, BasicInfo, Contact 
-# Languages, Courses, ProfessionalExperience, Skills, References
 from decouple import config
+from flask_jwt_extended import JWTManager
+from app.models import db, User, RevokedToken
 
-# Initialize Flask application
 app = Flask(__name__)
 
 # Configure database URI from environment variable
 app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = config('JWT_SECRET_KEY')  # Secret key for JWT
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 24 * 3600  # Set expiration time to 1 day (24 hours) in seconds
 
 # Initialize SQLAlchemy with the Flask app
 db.init_app(app)
 migrate = Migrate(app, db)
 
+# Register JWT extension with Flask app
+jwt = JWTManager(app)
+
+# importing blueprints
+from app.blueprints.auth_bp import auth_blueprint  # Import auth_blueprint from its file
+
+# Register authentication blueprint with Flask app
+app.register_blueprint(auth_blueprint)
+
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
